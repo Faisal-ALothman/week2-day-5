6d1a6a4e300b2bdff346b9361e6759362cedf484
Faisal-ALothman <110568112+Faisal-ALothman@users.noreply.github.com>
2022-08-14 10:29:47 +0300

    Add files via upload

---
+++ api.java
@@ -0,0 +1,12 @@
+package com.example.myuser;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor @Data
+public class api {
+
+    private String message;
+    private int status;
+
+}
